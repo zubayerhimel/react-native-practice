52e9ffbd604224fab199d5fe01d2b4efeaf5047a
zubayerhimel <zhimel007@gmail.com>
2021-07-24 11:18:15 +0600

    onk kichui korlam

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
-import { StatusBar } from "expo-status-bar";
+// import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks";
+import { StyleSheet, Platform, StatusBar, Dimensions, View } from "react-native";
+// import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <Text>My name is Zubayer</Text>
-
-      <StatusBar style="auto" />
-    </View>
-  );
+  // console.log(Dimensions.get("screen"));
+  const { landscape } = useDeviceOrientation();
+  return <ViewImageScreen />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
