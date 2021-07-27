bb427272efdf9fa80ca960cea28ba20066d3c132
zubayerhimel <zhimel007@gmail.com>
2021-07-27 23:10:36 +0600

    implemented icon component

--- App.js
+++ App.js
@@ -5,9 +5,15 @@ import MessagesScreen from "./app/screens/MessagesScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import CardComponent from "./components/CardComponent";
+import Icon from "./components/Icon";
+import Screen from "./components/Screen";
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name="email" size={50} backgroundColor="red" iconColor="white" />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
